a94c327d357b2ac0c0b0c195f93b8f961141b221
fartekool <levonychev2006@gmail.com>
2026-03-13 16:02:50 +0300

    added CHECK for all tests

--- clang/test/compiler-course/levonychev-i-lab1-test/test.cpp
+++ clang/test/compiler-course/levonychev-i-lab1-test/test.cpp
@@ -3,7 +3,6 @@
 
 
 // CHECK: void test_func1(const float{{\*}} const x1) {
-
 void test_func1(float* x1) {
     float var = *x1;
 }
@@ -25,6 +24,7 @@ void test_func3() {
 }
 
 // CHECK-NOT: int{{\*}} const x4_ptr = &x4;
+// CHECK: int{{\*}} x4_ptr = &x4;
 void test_func4() {
     int x4 = 0;
     int* x4_ptr = &x4;
@@ -40,25 +40,36 @@ void test_func5(double& x5) {
 
 
 // CHECK-NOT: void test_func6(const double& x6{{\)}} {
+// CHECK: void test_func6(double& x6{{\)}} {
 void test_func6(double& x6) {
     x6 = 4.0;
 }
 
 // CHECK-NOT: const int& x7 {{\=}} var;
+// CHECK: int& x7 {{\=}} var;
 void test_func7() {
     int var = 10;
     int& x7 = var;
     x7 = 100;
 }
 // CHECK-NOT: const int& x8 {{\=}} var;
+// CHECK: int& x8 {{\=}} var;
 void test_func8() {
     int var = 10;
     int& x8 = var;
     ++x8;
 }
 // CHECK-NOT: const int{{\*}} const x9 = &var;
+// CHECK: int{{\*}} const x9 = &var;
 void test_func9() {
     int var = 10;
     int* x9 = &var;
     ++(*x9);
+}
+
+// CHECK: const int{{\*}} x10 = &var; 
+void test_func10() {
+    int var = 100;
+    int* x10 = &var;
+    x10++;
 }
