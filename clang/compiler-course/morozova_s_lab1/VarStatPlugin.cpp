9b4feee94e1fff9dc63f5b697d27eac1b41ae92e
root <root@DESKTOP-7GBKN6H.localdomain>
2026-03-15 16:35:10 +0300

    fix gcc build

--- clang/compiler-course/morozova_s_lab1/VarStatPlugin.cpp
+++ clang/compiler-course/morozova_s_lab1/VarStatPlugin.cpp
@@ -9,75 +9,73 @@ using namespace clang;
 
 class VarStatVisitor : public RecursiveASTVisitor<VarStatVisitor> {
 public:
-    explicit VarStatVisitor(ASTContext *Context)
-        : globalCount(0), localCount(0), staticLocalCount(0), paramCount(0) {}
+  explicit VarStatVisitor(ASTContext *Context)
+      : globalCount(0), localCount(0), staticLocalCount(0), paramCount(0) {}
 
-    bool VisitVarDecl(VarDecl *VD) {
-        if (VD != VD->getCanonicalDecl())
-            return true;
-        if (!VD->isThisDeclarationADefinition())
-            return true;
+  bool VisitVarDecl(VarDecl *VD) {
+    if (VD != VD->getCanonicalDecl())
+      return true;
+    if (!VD->isThisDeclarationADefinition())
+      return true;
 
-        if (VD->hasGlobalStorage() && !VD->isStaticLocal())
-            globalCount++;
-        else if (VD->isLocalVarDecl() && !VD->isStaticLocal())
-            localCount++;
-        else if (VD->isStaticLocal())
-            staticLocalCount++;
+    if (VD->hasGlobalStorage() && !VD->isStaticLocal())
+      globalCount++;
+    else if (VD->isLocalVarDecl() && !VD->isStaticLocal())
+      localCount++;
+    else if (VD->isStaticLocal())
+      staticLocalCount++;
 
-        return true;
-    }
+    return true;
+  }
 
-    bool VisitParmVarDecl(ParmVarDecl *PD) {
-        paramCount++;
-        return true;
-    }
+  bool VisitParmVarDecl(ParmVarDecl *PD) {
+    paramCount++;
+    return true;
+  }
 
-    void print() {
-        llvm::outs() << "========== СТАТИСТИКА ПЕРЕМЕННЫХ ==========\n";
-        llvm::outs() << "Глобальных переменных: " << globalCount << "\n";
-        llvm::outs() << "Локальных переменных: " << localCount << "\n";
-        llvm::outs() << "Статических локальных: " << staticLocalCount << "\n";
-        llvm::outs() << "Параметров функций: " << paramCount << "\n";
-        llvm::outs() << "===========================================\n";
-    }
+  void print() {
+    llvm::outs() << "========== СТАТИСТИКА ПЕРЕМЕННЫХ ==========\n";
+    llvm::outs() << "Глобальных переменных: " << globalCount << "\n";
+    llvm::outs() << "Локальных переменных: " << localCount << "\n";
+    llvm::outs() << "Статических локальных: " << staticLocalCount << "\n";
+    llvm::outs() << "Параметров функций: " << paramCount << "\n";
+    llvm::outs() << "===========================================\n";
+  }
 
 private:
-    int globalCount;
-    int localCount;
-    int staticLocalCount;
-    int paramCount;
+  int globalCount;
+  int localCount;
+  int staticLocalCount;
+  int paramCount;
 };
 
 class VarStatConsumer : public ASTConsumer {
 public:
-    explicit VarStatConsumer(ASTContext *Context) : Visitor(Context) {}
+  explicit VarStatConsumer(ASTContext *Context) : Visitor(Context) {}
 
-    void HandleTranslationUnit(ASTContext &Context) override {
-        Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-        Visitor.print();
-    }
+  void HandleTranslationUnit(ASTContext &Context) override {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+    Visitor.print();
+  }
 
 private:
-    VarStatVisitor Visitor;
+  VarStatVisitor Visitor;
 };
 
 class VarStatAction : public PluginASTAction {
 public:
-    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
-                                                   StringRef InFile) override {
-        return std::make_unique<VarStatConsumer>(&CI.getASTContext());
-    }
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                 StringRef InFile) override {
+    return std::make_unique<VarStatConsumer>(&CI.getASTContext());
+  }
 
-    bool ParseArgs(const CompilerInstance &CI,
-                   const std::vector<std::string> &args) override {
-        return true;
-    }
+  bool ParseArgs(const CompilerInstance &CI,
+                 const std::vector<std::string> &args) override {
+    return true;
+  }
 
-    ActionType getActionType() override {
-        return AddBeforeMainAction;
-    }
+  ActionType getActionType() override { return AddBeforeMainAction; }
 };
 
 static FrontendPluginRegistry::Add<VarStatAction>
-X("var-stat", "variable statistics plugin");
+    X("var-stat", "variable statistics plugin");
--- clang/test/compiler-course/morozova_s_lab1_test/test1.cpp
+++ clang/test/compiler-course/morozova_s_lab1_test/test1.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/VarStatPlugin%shlibext -plugin var-stat -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -load %llvmshlibdir/VarStatPlugin.so -plugin var-stat -fsyntax-only %s 2>&1 | FileCheck %s
 
 // CHECK: ========== СТАТИСТИКА ПЕРЕМЕННЫХ ==========
 // CHECK: Глобальных переменных: 23
