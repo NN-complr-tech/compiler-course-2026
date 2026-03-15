83a62d06707823a6b7c43cc80efda7700f14cb39
root <root@DESKTOP-7GBKN6H.localdomain>
2026-03-15 15:53:21 +0300

    fix gcc build

--- clang/test/compiler-course/morozova_s_lab1_test/test1.cpp
+++ clang/test/compiler-course/morozova_s_lab1_test/test1.cpp
@@ -1,4 +1,4 @@
-// RUN: %llvm_tools_dir/my_tool %s | FileCheck %s
+// RUN: %build/bin/my_tool %s | FileCheck %s
 
 // CHECK: ========== СТАТИСТИКА ПЕРЕМЕННЫХ ==========
 // CHECK: Глобальных переменных: 23
