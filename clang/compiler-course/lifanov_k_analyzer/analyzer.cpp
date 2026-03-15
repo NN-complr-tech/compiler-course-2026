e232d883322065ab352e43df21c15b61fa8dd163
LeeFoS6092 <LeeFoS6092>
2026-03-15 12:53:10 +0300

    Fix format and string

--- clang/compiler-course/lifanov_k_analyzer/analyzer.cpp
+++ clang/compiler-course/lifanov_k_analyzer/analyzer.cpp
@@ -3,7 +3,6 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 
-
 #include <unordered_set>
 #include "llvm/Support/raw_ostream.h"
 
@@ -13,143 +12,158 @@ namespace {
 
 class ResourceVisitor : public RecursiveASTVisitor<ResourceVisitor> {
 public:
-    explicit ResourceVisitor(ASTContext &ctx) : context(ctx) {}
+  explicit ResourceVisitor(ASTContext &ctx) : context(ctx) {}
 
-    bool VisitVarDecl(VarDecl *vd) {
-        if (!vd->hasInit())
-            return true;
+  bool VisitVarDecl(VarDecl *vd) {
+    if (!vd->hasInit())
+      return true;
 
-        Expr *init = vd->getInit()->IgnoreParenCasts();
+    Expr *init = vd->getInit()->IgnoreParenCasts();
 
-        if (isAllocation(init)) {
-            allocated.insert(vd);
-        }
-
-        return true;
+    if (isAllocation(init)) {
+      allocated.insert(vd);
     }
 
-    bool VisitBinaryOperator(BinaryOperator *op) {
-        if (!op->isAssignmentOp())
-            return true;
+    return true;
+  }
 
-        Expr *rhs = op->getRHS()->IgnoreParenCasts();
-        if (!isAllocation(rhs))
-            return true;
+  bool VisitBinaryOperator(BinaryOperator *op) {
+    if (!op->isAssignmentOp())
+      return true;
 
-        VarDecl *vd = extractVar(op->getLHS());
-        if (vd) allocated.insert(vd);
+    Expr *rhs = op->getRHS()->IgnoreParenCasts();
+    if (!isAllocation(rhs))
+      return true;
 
-        return true;
+    VarDecl *vd = extractVar(op->getLHS());
+    if (vd)
+      allocated.insert(vd);
+
+    return true;
+  }
+
+  bool VisitCallExpr(CallExpr *call) {
+    FunctionDecl *callee = call->getDirectCallee();
+    if (!callee)
+      return true;
+
+    StringRef name = callee->getName();
+
+    if (name == "free" || name == "fclose") {
+      Expr *arg = call->getArg(0)->IgnoreParenCasts();
+      VarDecl *vd = extractVar(arg);
+      if (vd)
+        released.insert(vd);
     }
 
-    bool VisitCallExpr(CallExpr *call) {
-        FunctionDecl *callee = call->getDirectCallee();
-        if (!callee)
-            return true;
+    return true;
+  }
 
-        StringRef name = callee->getName();
+  bool VisitCXXDeleteExpr(CXXDeleteExpr *expr) {
+    Expr *arg = expr->getArgument()->IgnoreParenCasts();
+    VarDecl *vd = extractVar(arg);
+    if (vd)
+      released.insert(vd);
 
-        if (name == "free" || name == "fclose") {
-            Expr *arg = call->getArg(0)->IgnoreParenCasts();
-            VarDecl *vd = extractVar(arg);
-            if (vd) released.insert(vd);
-        }
+    return true;
+  }
 
-        return true;
+  bool VisitReturnStmt(ReturnStmt *ret) {
+    for (auto *var : allocated) {
+      if (released.find(var) == released.end() &&
+          reported.find(var) == reported.end()) {
+        warnReturn(ret->getReturnLoc());
+        reported.insert(var);
+      }
     }
+    return true;
+  }
 
-    bool VisitCXXDeleteExpr(CXXDeleteExpr *expr) {
-        Expr *arg = expr->getArgument()->IgnoreParenCasts();
-        VarDecl *vd = extractVar(arg);
-        if (vd) released.insert(vd);
-
-        return true;
+  void finalize() {
+    for (auto *var : allocated) {
+      if (released.find(var) == released.end() &&
+          reported.find(var) == reported.end()) {
+        warnFinalize(var->getLocation());
+      }
     }
-
-    bool VisitReturnStmt(ReturnStmt *ret) {
-        for (auto *var : allocated) {
-            if (released.find(var) == released.end() && reported.find(var) == reported.end()) {
-                warn(ret->getReturnLoc(), "ресурс для переменной '%0' может быть не освобождён при выходе");
-                reported.insert(var);
-            }
-        }
-        return true;
-    }
-
-    void finalize() {
-        for (auto *var : allocated) {
-            if (released.find(var) == released.end() && reported.find(var) == reported.end()) {
-                warn(var->getLocation(), "выделенный ресурс для '%0' не освобождён");
-            }
-        }
-    }
+  }
 
 private:
-    bool isAllocation(Expr *expr) {
-        if (!expr) return false;
+  bool isAllocation(Expr *expr) {
+    if (!expr)
+      return false;
 
-        if (isa<CXXNewExpr>(expr)) return true;
+    if (isa<CXXNewExpr>(expr))
+      return true;
 
-        if (auto *call = dyn_cast<CallExpr>(expr)) {
-            if (auto *callee = call->getDirectCallee()) {
-                StringRef name = callee->getName();
-                return name == "malloc" || name == "fopen";
-            }
-        }
-
-        return false;
+    if (auto *call = dyn_cast<CallExpr>(expr)) {
+      if (auto *callee = call->getDirectCallee()) {
+        StringRef name = callee->getName();
+        return name == "malloc" || name == "fopen";
+      }
     }
 
-    VarDecl *extractVar(Expr *expr) {
-        expr = expr->IgnoreParenCasts();
+    return false;
+  }
 
-        if (auto *ref = dyn_cast<DeclRefExpr>(expr)) return dyn_cast<VarDecl>(ref->getDecl());
+  VarDecl *extractVar(Expr *expr) {
+    expr = expr->IgnoreParenCasts();
 
-        return nullptr;
-    }
+    if (auto *ref = dyn_cast<DeclRefExpr>(expr))
+      return dyn_cast<VarDecl>(ref->getDecl());
 
-    void warn(SourceLocation loc, const char *msg) {
-        DiagnosticsEngine &diag = context.getDiagnostics();
+    return nullptr;
+  }
 
-        unsigned id = diag.getCustomDiagID(
-            DiagnosticsEngine::Warning, msg);
+  void warnReturn(SourceLocation loc) {
+      DiagnosticsEngine &diag = context.getDiagnostics();
+      unsigned id = diag.getCustomDiagID(
+          DiagnosticsEngine::Warning,
+          "ресурс для переменной '%0' может быть не освобождён при выходе");
+      diag.Report(loc, id);
+  }
 
-        diag.Report(loc, id);
-    }
+  void warnFinalize(SourceLocation loc) {
+    DiagnosticsEngine &diag = context.getDiagnostics();
+    unsigned id = diag.getCustomDiagID(
+        DiagnosticsEngine::Warning,
+        "выделенный ресурс для '%0' не освобождён");
+    diag.Report(loc, id);
+  }
 
-    ASTContext &context;
-
-    std::unordered_set<VarDecl*> allocated;
-    std::unordered_set<VarDecl*> released;
-    std::unordered_set<VarDecl*> reported;
+  ASTContext &context;
+  std::unordered_set<VarDecl *> allocated;
+  std::unordered_set<VarDecl *> released;
+  std::unordered_set<VarDecl *> reported;
 };
 
 class ResourceConsumer : public ASTConsumer {
 public:
-    explicit ResourceConsumer(ASTContext &ctx) : visitor(ctx) {}
+  explicit ResourceConsumer(ASTContext &ctx) : visitor(ctx) {}
 
-    void HandleTranslationUnit(ASTContext &ctx) override {
-        visitor.TraverseDecl(ctx.getTranslationUnitDecl());
-        visitor.finalize();
-    }
+  void HandleTranslationUnit(ASTContext &ctx) override {
+    visitor.TraverseDecl(ctx.getTranslationUnitDecl());
+    visitor.finalize();
+  }
 
-    private:
-        ResourceVisitor visitor;
+private:
+  ResourceVisitor visitor;
 };
 
 class ResourcePluginAction : public PluginASTAction {
-    protected:
-        std::unique_ptr<ASTConsumer>
-        CreateASTConsumer(CompilerInstance &ci, llvm::StringRef) override {
-            return std::make_unique<ResourceConsumer>(ci.getASTContext());
-        }
+protected:
+  std::unique_ptr<ASTConsumer>
+  CreateASTConsumer(CompilerInstance &ci, llvm::StringRef) override {
+    return std::make_unique<ResourceConsumer>(ci.getASTContext());
+  }
 
-        bool ParseArgs(const CompilerInstance &, const std::vector<std::string> &) override {
-            return true;
-        }
-    };
+  bool ParseArgs(const CompilerInstance &, const std::vector<std::string> &) override {
+    return true;
+  }
+};
 
 } // namespace
 
 static FrontendPluginRegistry::Add<ResourcePluginAction>
-    X("lifanov_k_resource-checker", "detects missing delete/free/fclose");
+    X("lifanov_k_resource-checker",
+      "detects missing delete/free/fclose");
