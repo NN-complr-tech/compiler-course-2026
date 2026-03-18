1cc418bb702ecf1d69e7cee542d5863ce738addd
Nikita-Ovsyannikov <nik.ovsyannikov2090@gmail.com>
2026-03-18 19:53:34 +0300

    add tests

--- clang/test/compiler-course/ovsyannikov_n_lab1/test.cpp
+++ clang/test/compiler-course/ovsyannikov_n_lab1/test.cpp
@@ -2,14 +2,54 @@
 
 class Base {
 public:
-    virtual void foo() {}
-    virtual void bar() {}
+    virtual void func1() {}
+    virtual void func2(int x) {}
     virtual ~Base() {}
+};
+
+class Interface {
+public:
+    virtual void action() = 0;
 };
 
 class Derived : public Base {
 public:
-    void foo() {} // expected-warning {{ovsyannikov-check: method 'foo' overrides a virtual function but lacks 'override' specifier}}
-    
-    void bar() override {} 
+    void func1() {} // expected-warning {{ovsyannikov-check: method 'func1' overrides a virtual function but lacks 'override' specifier}}
+
+    void func2(int x) override {}
+
+    ~Derived() {} // expected-warning {{ovsyannikov-check: method '~Derived' overrides a virtual function but lacks 'override' specifier}}
+};
+
+class GrandDerived : public Derived {
+public:
+    void func1() override {}
+
+    void func2(int x) {} // expected-warning {{ovsyannikov-check: method 'func2' overrides a virtual function but lacks 'override' specifier}}
+};
+
+class MultiDerived : public Base, public Interface {
+public:
+    void action() {} // expected-warning {{ovsyannikov-check: method 'action' overrides a virtual function but lacks 'override' specifier}}
+
+    void func1() override {}
+};
+
+class Independent {
+public:
+    void someMethod() {}
+
+    virtual void startVirtual() {}
+};
+
+class Overloader : public Base {
+public:
+    void func2(double d) {}
+
+    void myOwnMethod() {}
+};
+
+class FinalTest : public Base {
+public:
+    void func1() final {} // expected-warning {{ovsyannikov-check: method 'func1' overrides a virtual function but lacks 'override' specifier}}
 };
