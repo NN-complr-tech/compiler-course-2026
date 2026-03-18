cd11c7784ff9d87a8b1ed30377fa97f2b705c17d
Pylaeva Svetlana <pl.svetlanaa.a@gmail.com>
2026-03-18 17:00:56 +0300

    fix tests and source

--- clang/compiler-course/pylaeva_s_lab1/source.cpp
+++ clang/compiler-course/pylaeva_s_lab1/source.cpp
@@ -8,10 +8,10 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "llvm/Support/raw_ostream.h"
+#include <cstdio>
 #include <map>
 #include <set>
 #include <string>
-#include <cstdio>
 
 namespace {
 
@@ -57,90 +57,90 @@ public:
       return true;
 
     if (name == "malloc" || name == "calloc" || name == "realloc" ||
-    name == "fopen") {
-  std::string varName = "unknown";
+        name == "fopen") {
+      std::string varName = "unknown";
 
-  // Ищем присваивание или инициализацию с учетом приведения типов
-    const clang::Stmt *currentStmt = call;
-    auto parents = m_context->getParents(*currentStmt);
-    
-    while (!parents.empty()) {
+      // Ищем присваивание или инициализацию с учетом приведения типов
+      const clang::Stmt *currentStmt = call;
+      auto parents = m_context->getParents(*currentStmt);
+
+      while (!parents.empty()) {
         if (auto *castExpr = parents.begin()[0].get<clang::CStyleCastExpr>()) {
-            currentStmt = castExpr;
-            parents = m_context->getParents(*currentStmt);
-            continue;
+          currentStmt = castExpr;
+          parents = m_context->getParents(*currentStmt);
+          continue;
         }
-        
+
         if (auto *binaryOp = parents.begin()[0].get<clang::BinaryOperator>()) {
-            if (binaryOp->getOpcode() == clang::BO_Assign) {
-                if (auto *declRef = llvm::dyn_cast<clang::DeclRefExpr>(
-                        binaryOp->getLHS()->IgnoreImpCasts())) {
-                    if (auto *varDecl =
-                            llvm::dyn_cast<clang::VarDecl>(declRef->getDecl())) {
-                        varName = varDecl->getNameAsString();
-                    }
-                }
+          if (binaryOp->getOpcode() == clang::BO_Assign) {
+            if (auto *declRef = llvm::dyn_cast<clang::DeclRefExpr>(
+                    binaryOp->getLHS()->IgnoreImpCasts())) {
+              if (auto *varDecl =
+                      llvm::dyn_cast<clang::VarDecl>(declRef->getDecl())) {
+                varName = varDecl->getNameAsString();
+              }
             }
-            break;
+          }
+          break;
         }
-        
+
         // проверка на VarDecl (для инициализации при объявлении)
         if (auto *varDecl = parents.begin()[0].get<clang::VarDecl>()) {
-            varName = varDecl->getNameAsString();
-            break;
+          varName = varDecl->getNameAsString();
+          break;
         }
-        
+
         break;
-    }
+      }
 
-  if (name == "fopen") {
-    m_resources.insert({loc, "file", varName});
-  } else {
-    m_resources.insert({loc, "memory", varName});
-  }
-} else if (name == "free") {
-  // При освобождении ресурса ищем по типу и области видимости
-  std::string varName = "unknown";
-  
-  // Пытаемся найти имя переменной, которая освобождается
-  if (auto *declRef = llvm::dyn_cast<clang::DeclRefExpr>(
-          call->getArg(0)->IgnoreImpCasts())) {
-    if (auto *varDecl =
-            llvm::dyn_cast<clang::VarDecl>(declRef->getDecl())) {
-      varName = varDecl->getNameAsString();
-    }
-  }
+      if (name == "fopen") {
+        m_resources.insert({loc, "file", varName});
+      } else {
+        m_resources.insert({loc, "memory", varName});
+      }
+    } else if (name == "free") {
+      // При освобождении ресурса ищем по типу и области видимости
+      std::string varName = "unknown";
 
-  auto it = m_resources.begin();
-  while (it != m_resources.end()) {
-    if (it->type == "memory" && it->varName == varName) {
-      it = m_resources.erase(it);
-      break;
-    } else {
-      ++it;
-    }
-  }
-} else if (name == "fclose") {
-  std::string varName = "unknown";
-  
-  if (auto *declRef = llvm::dyn_cast<clang::DeclRefExpr>(
-          call->getArg(0)->IgnoreImpCasts())) {
-    if (auto *varDecl =
-            llvm::dyn_cast<clang::VarDecl>(declRef->getDecl())) {
-      varName = varDecl->getNameAsString();
-    }
-  }
+      // Пытаемся найти имя переменной, которая освобождается
+      if (auto *declRef = llvm::dyn_cast<clang::DeclRefExpr>(
+              call->getArg(0)->IgnoreImpCasts())) {
+        if (auto *varDecl =
+                llvm::dyn_cast<clang::VarDecl>(declRef->getDecl())) {
+          varName = varDecl->getNameAsString();
+        }
+      }
 
-  auto it = m_resources.begin();
-  while (it != m_resources.end()) {
-    if (it->type == "file" && it->varName == varName) {
-      it = m_resources.erase(it);
-      break;
-    } else {
-      ++it;
+      auto it = m_resources.begin();
+      while (it != m_resources.end()) {
+        if (it->type == "memory" && it->varName == varName) {
+          it = m_resources.erase(it);
+          break;
+        } else {
+          ++it;
+        }
+      }
+    } else if (name == "fclose") {
+      std::string varName = "unknown";
+
+      if (auto *declRef = llvm::dyn_cast<clang::DeclRefExpr>(
+              call->getArg(0)->IgnoreImpCasts())) {
+        if (auto *varDecl =
+                llvm::dyn_cast<clang::VarDecl>(declRef->getDecl())) {
+          varName = varDecl->getNameAsString();
+        }
+      }
+
+      auto it = m_resources.begin();
+      while (it != m_resources.end()) {
+        if (it->type == "file" && it->varName == varName) {
+          it = m_resources.erase(it);
+          break;
+        } else {
+          ++it;
+        }
+      }
     }
-  }
-}
 
     return true;
   }
@@ -184,8 +184,7 @@ public:
       if (auto *varDecl = llvm::dyn_cast<clang::VarDecl>(declRef->getDecl())) {
         std::string varName = varDecl->getNameAsString();
 
-        // Ищем ресурс с таким же именем переменной, типом "memory" и областью
-        // видимости
+        // Ищем ресурс с таким же именем переменной, типом "memory"
         auto it = m_resources.begin();
         while (it != m_resources.end()) {
           if (it->type == "memory" && it->varName == varName) {
--- clang/test/compiler-course/pylaeva_s_lab1/test.cpp
+++ clang/test/compiler-course/pylaeva_s_lab1/test.cpp
@@ -1,4 +1,8 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/pylaeva_s_lab1_ClangAST%pluginext -plugin pylaeva_s_lab1_plugin -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: split-file %s %t
+// RUN: %clang_cc1 -load %llvmshlibdir/pylaeva_s_lab1_ClangAST%pluginext -plugin pylaeva_s_lab1_plugin -fsyntax-only %t/leak.cpp 2>&1 | FileCheck %t/leak.cpp
+// RUN: %clang_cc1 -load %llvmshlibdir/pylaeva_s_lab1_ClangAST%pluginext -plugin pylaeva_s_lab1_plugin -fsyntax-only %t/no_leak.cpp 2>&1 | FileCheck %t/no_leak.cpp --allow-empty
+
+//--- leak.cpp
 
 extern "C" {
     void* malloc(unsigned long size);
@@ -11,79 +15,56 @@ extern "C" {
 
 // Проверка обнаружения простых утечек памяти внутри функций
 
-// CHECK: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+2]]
 void test_calloc_simple_leak() {
-    int *p = (int*)calloc(10, sizeof(int));
+    int *p_simple_calloc = (int*)calloc(10, sizeof(int));
 }
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+2]]
 void test_new_simple_leak() {
-    int* p = new int;
+    int* p_simple_new = new int;
 }
 
-// CHECK-DAG: warning: potential file handle leak detected at line [[#]]
+// CHECK: warning: potential file handle leak detected at line [[#@LINE+2]]
 void test_fopen_simple_leak() {
-    void* f = fopen("test.txt", "r");
+    void* f_simple = fopen("test.txt", "r");
 }
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+2]]
 void test_malloc_simple_leak() {
-    int* p = (int*)malloc(sizeof(int));
+    int* p_simple_malloc = (int*)malloc(sizeof(int));
 }
-
 
 // Проверка обнаружения простых утечек памяти на глобальном уровне
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+1]]
 int* leak_malloc = (int*)malloc(100);
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+1]]
 int* leak_сalloc = (int*)calloc(10, sizeof(int));
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+1]]
 int* leak_realloc = (int*)realloc(nullptr, 200);
 
-// CHECK-DAG: warning: potential file handle leak detected at line [[#]]
+// CHECK: warning: potential file handle leak detected at line [[#@LINE+1]]
 void* file = fopen("test.txt", "r");
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+1]]
 int* leak_new1 = new int(42);
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+1]]
 int* leak_new2 = new int[100];
-
-
-// Разные области видимости с одинаковыми именами переменных
-
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
-void scope_test1() {
-    int* p = (int*)malloc(10);  
-    {
-        int* p = (int*)malloc(20);  
-        free(p);  
-    }  
-}
-
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
-void scope_test2() {
-    int* p = (int*)malloc(10);
-    free(p);  
-    {
-        
-        int* p = (int*)malloc(20);
-    }
-}
 
 // Тесты на перезапись указателей
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+3]]
 void overwrite_test() {
     int* p = (int*)malloc(10);
     p = (int*)malloc(20); 
     free(p); 
 }
 
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+3]]
 void overwrite_correct() {
     int* p1 = (int*)malloc(10);
     int* p2 = (int*)malloc(20);
@@ -93,20 +74,20 @@ void overwrite_correct() {
 }
 
 // Выделение памяти в сложном выражении
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+2]]
 void complex_expression_leak() {
     int* p = (int*)malloc(sizeof(int) * (10 + 20));
 }
 
 // Множественное выделение в одной строке
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+2]]
 void multiple_allocation_same_line() {
     int* a = (int*)malloc(10), *b = (int*)malloc(20);
     free(a); 
 }
 
 // Утечка памяти в if
-// CHECK-DAG: warning: potential memory leak detected at line [[#]]
+// CHECK: warning: potential memory leak detected at line [[#@LINE+3]]
 void if_leak(int x) {
     if (x > 0) {
         int* p = (int*)malloc(10);
@@ -116,45 +97,52 @@ void if_leak(int x) {
 // Выделение памяти в цикле, утечка в каждой итерации
 void loop_leak() {
     for (int i = 0; i < 5; i++) {
-        // CHECK-DAG: warning: potential memory leak detected at line [[#]]
+        // CHECK: warning: potential memory leak detected at line [[#@LINE+1]]
         int* p = (int*)malloc(10);
     }
 }
 
-// тесты без утечек
 
 
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
+//--- no_leak.cpp
+
+extern "C" {
+    void* malloc(unsigned long size);
+    void* calloc(unsigned long count, unsigned long size);
+    void* realloc(void* ptr, unsigned long size);
+    void free(void* ptr);
+    void* fopen(const char* filename, const char* mode);
+    int fclose(void* stream);
+}
+
+// CHECK-NOT: warning: potential memory leak
+// CHECK-NOT: warning: potential file handle leak
+
 void correct_malloc() {
     int* p = (int*)malloc(100);
     free(p);
 }
 
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
 void correct_calloc() {
     int* p = (int*)calloc(10, sizeof(int));
     free(p);
 }
 
-// CHECK-NOT: warning: potential file handle leak detected at line [[#]]
 void correct_fopen() {
     void* f = fopen("test.txt", "r");
     fclose(f);
 }
 
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
 void correct_new1() {
     int* p = new int(64);
     delete p;
 }
 
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
 void correct_new2() {
     int* p = new int[10];
     delete[] p;
 }
 
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
 void correct_multiple() {
     int* a = (int*)malloc(10);
     int* b = new int(5);
@@ -164,7 +152,6 @@ void correct_multiple() {
     fclose(f);
 }
 
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
 void correct_if(int x) {
     int* p = (int*)malloc(10);
     if (x > 0) {
@@ -174,16 +161,9 @@ void correct_if(int x) {
     }
 }
 
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
 void loop_correct() {
     for (int i = 0; i < 5; i++) {
         int* p = (int*)malloc(10);
         free(p);
     }
-}
-
-// CHECK-NOT: warning: potential memory leak detected at line [[#]]
-int* correct_return(int x) {
-    int* p = new int(x);
-    return p;
 }
