8191a1badd1cdbf3408370632717c92abcdf435f
roman-pikhotskiy <romapanda5@mail.ru>
2026-03-20 21:31:44 +0300

    retry

--- clang/compiler-course/pikhotskiy_r_lab1/source.cpp
+++ clang/compiler-course/pikhotskiy_r_lab1/source.cpp
@@ -15,16 +15,19 @@ public:
     if (var != var->getCanonicalDecl())
       return true;
 
-    if (clang::isa<clang::ParmVarDecl>(var))
+    if (var->hasExternalStorage() && !var->isThisDeclarationADefinition())
       return true;
 
-    if (var->getStorageClass() == clang::SC_Static || var->isStaticLocal() ||
-        var->isStaticDataMember()) {
-      statics++;
+    if (var->isFileVarDecl()) {
+      if (var->getStorageClass() == clang::SC_Static)
+        statics++;
+      else
+        globals++;
     } else if (var->isLocalVarDecl()) {
-      locals++;
-    } else if (var->isFileVarDecl()) {
-      globals++;
+      if (var->isStaticLocal())
+        statics++;
+      else
+        locals++;
     }
 
     return true;
@@ -36,11 +39,11 @@ public:
   }
 
   void printResults() {
-    llvm::errs() << "Global variables: " << globals << "\n";
-    llvm::errs() << "Local variables: " << locals << "\n";
-    llvm::errs() << "Static variables: " << statics << "\n";
-    llvm::errs() << "Function parameters: " << params << "\n";
-    llvm::errs() << "Total: " << globals + locals + statics + params << "\n";
+    llvm::outs() << "Global variables: " << globals << "\n";
+    llvm::outs() << "Local variables: " << locals << "\n";
+    llvm::outs() << "Static variables: " << statics << "\n";
+    llvm::outs() << "Function parameters: " << params << "\n";
+    llvm::outs() << "Total: " << globals + locals + statics + params << "\n";
   }
 
 private:
@@ -52,7 +55,8 @@ private:
 
 class VarCounterConsumer : public clang::ASTConsumer {
 public:
-  explicit VarCounterConsumer(clang::ASTContext *context) : visitor(context) {}
+  explicit VarCounterConsumer(clang::ASTContext *context)
+      : visitor(context) {}
 
   void HandleTranslationUnit(clang::ASTContext &context) override {
     visitor.TraverseDecl(context.getTranslationUnitDecl());
@@ -74,12 +78,8 @@ public:
                  const std::vector<std::string> &args) override {
     return true;
   }
-
-  PluginASTAction::ActionType getActionType() override {
-    return AddAfterMainAction;
-  }
 };
-} // namespace
+}
 
 static clang::FrontendPluginRegistry::Add<VarCounterAction>
     X("var_counter_plugin", "counts different types of variables");
--- clang/test/compiler-course/pikhotskiy_r_lab1_tests/test.cpp
+++ clang/test/compiler-course/pikhotskiy_r_lab1_tests/test.cpp
@@ -1,60 +1,62 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/pikhotskiy_r_lab1_ClangAST%pluginext -plugin var_counter_plugin -fsyntax-only %s 2>&1 | FileCheck %s
 
-// CHECK: Global variables: 8
+// CHECK: Global variables: 7
 // CHECK-NEXT: Local variables: 7
 // CHECK-NEXT: Static variables: 7
 // CHECK-NEXT: Function parameters: 5
-// CHECK-NEXT: Total: 27
+// CHECK-NEXT: Total: 26
 
-int global1 = 10;                 // global +1
-int global2;                       // global +1
-extern int global3;                // global +1
+int global1 = 10;
+int global2;
 
-static int static_global1 = 5;     // static +1
-static int static_global2;          // static +1
+static int static_global1 = 5;
+static int static_global2;
 
 namespace {
-    int anon_global1;               // global +1
-    static int anon_static1;         // static +1
+    int anon_global1;
+    static int anon_static1;
 }
 
 namespace MyNS {
-    int ns_global;                   // global +1
-    static int ns_static;            // static +1
+    int ns_global;
+    static int ns_static;
 }
 
-int func1(int a, int b) {           // params +2
-    int local1 = 0;                  // local +1
-    static int static_local1 = 0;     // static +1
+extern int extern_global;
+int extern_global = 42;
+
+int func1(int a, int b) {
+    int local1 = 0;
+    static int static_local1 = 0;
     return local1;
 }
 
-double func2(double x) {             // params +1
-    int local2 = 42;                  // local +1
+double func2(double x) {
+    int local2 = 42;
     {
-        int block_local = 100;         // local +1
-        static int block_static = 200;  // static +1
+        int block_local = 100;
+        static int block_static = 200;
     }
     return x;
 }
 
 class Test {
-    int field;                        // не считаем
-    static int static_field;           // static +1
+    int field;
+    static int static_field;
 public:
-    Test(int p) {                      // params +1
-        int local3 = 1;                  // local +1
+    Test(int p) {
+        int local3 = 1;
     }
-    void method(int p1, float p2) {    // params +2
-        int local4 = 2;                   // local +1
-        static int method_static = 0;      // static +1
+    void method(int p1, float p2) {
+        int local4 = 2;
+        static int method_static = 0;
     }
 };
 
-int Test::static_field = 0;            // static +1
+int Test::static_field = 0;
 
-int main(int argc, char* argv[]) {     // params +2
-    int local5 = 3;                      // local +1
-    static int main_static = 4;           // static +1
+int main(int argc, char* argv[]) {
+    int local5 = 3;
+    static int main_static = 4;
     return 0;
 }
