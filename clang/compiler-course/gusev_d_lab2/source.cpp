8fb3751d601cad5fb35e1d8d69119118740cb7be
Дмитрий Гусев <144247998+userskins@users.noreply.github.com>
2026-03-23 21:18:10 +0300

    Source part (not done)

---
+++ clang/compiler-course/gusev_d_lab2/source.cpp
@@ -0,0 +1,87 @@
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+
+namespace {
+class GusevDLab2Pass : public llvm::PassInfoMixin<GusevDLab2Pass> {
+public:
+  llvm::PreservedAnalyses run(llvm::Function &Function,
+                              llvm::FunctionAnalysisManager &) {
+    llvm::SmallVector<llvm::BinaryOperator *, 8> Remainders;
+
+    for (llvm::BasicBlock &Block : Function) {
+      for (llvm::Instruction &Instruction : Block) {
+        auto *BinaryOp = llvm::dyn_cast<llvm::BinaryOperator>(&Instruction);
+        if (BinaryOp == nullptr) {
+          continue;
+        }
+
+        switch (BinaryOp->getOpcode()) {
+        case llvm::Instruction::SRem:
+        case llvm::Instruction::URem:
+        case llvm::Instruction::FRem:
+          Remainders.push_back(BinaryOp);
+          break;
+        default:
+          break;
+        }
+      }
+    }
+
+    if (Remainders.empty()) {
+      return llvm::PreservedAnalyses::all();
+    }
+
+    for (llvm::BinaryOperator *Remainder : Remainders) {
+      decomposeRemainder(*Remainder);
+    }
+
+    return llvm::PreservedAnalyses::none();
+  }
+
+  static bool isRequired() { return true; }
+
+private:
+  static void decomposeRemainder(llvm::BinaryOperator &Remainder) {
+    llvm::Value *Dividend = Remainder.getOperand(0);
+    llvm::Value *Divisor = Remainder.getOperand(1);
+
+    llvm::IRBuilder<> Builder(&Remainder);
+    Builder.SetCurrentDebugLocation(Remainder.getDebugLoc());
+
+    llvm::Value *Quotient = nullptr;
+    llvm::Value *Product = nullptr;
+    llvm::Value *Replacement = nullptr;
+
+    switch (Remainder.getOpcode()) {
+    case llvm::Instruction::SRem:
+      Quotient = Builder.CreateSDiv(Dividend, Divisor, "rem.div");
+      Product = Builder.CreateMul(Quotient, Divisor, "rem.mul");
+      Replacement = Builder.CreateSub(Dividend, Product, "rem.sub");
+      break;
+    case llvm::Instruction::URem:
+      Quotient = Builder.CreateUDiv(Dividend, Divisor, "rem.div");
+      Product = Builder.CreateMul(Quotient, Divisor, "rem.mul");
+      Replacement = Builder.CreateSub(Dividend, Product, "rem.sub");
+      break;
+    case llvm::Instruction::FRem: {
+      Builder.setFastMathFlags(Remainder.getFastMathFlags());
+      Quotient = Builder.CreateFDiv(Dividend, Divisor, "rem.div");
+      Product = Builder.CreateFMul(Quotient, Divisor, "rem.mul");
+      Replacement = Builder.CreateFSub(Dividend, Product, "rem.sub");
+      break;
+    }
+    default:
+      return;
+    }
+
+    Remainder.replaceAllUsesWith(Replacement);
+    Remainder.eraseFromParent();
+  }
+};
+} // namespace
