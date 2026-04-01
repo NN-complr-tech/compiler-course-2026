c5c57143f80e333fc9eb218b97bb9645c576fdbb
ulillli <yuliyapap1510@gmail.com>
2026-04-01 22:19:29 +0300

    second lab implementation

--- llvm/compiler-course/llvm-ir/papulina_yuliya_fi3/MulDelOptimization.cpp
+++ llvm/compiler-course/llvm-ir/papulina_yuliya_fi3/MulDelOptimization.cpp
@@ -9,23 +9,58 @@ namespace {
 struct MulDelOptimization : llvm::PassInfoMixin<MulDelOptimization> {
   llvm::PreservedAnalyses run(llvm::Function &func,
                               llvm::FunctionAnalysisManager &) {
+    bool isChanged = false;
     for (llvm::BasicBlock &bb : func) {
       for (llvm::Instruction &instr : llvm::make_early_inc_range(bb)) {
         if (llvm::BinaryOperator *binOp =
                 llvm::dyn_cast<llvm::BinaryOperator>(&instr)) {
+          if (binOp->getOpcode() != llvm::Instruction::Mul &&
+              binOp->getOpcode() != llvm::Instruction::UDiv &&
+              binOp->getOpcode() != llvm::Instruction::SDiv)
+            continue;
+          llvm::Value *rhs = binOp->getOperand(1);
+
+          auto *C = llvm::dyn_cast<llvm::ConstantInt>(rhs);
+          if (!C)
+            continue;
+          if (!C->getValue().isPowerOf2() ||
+              !C->getValue().isStrictlyPositive())
+            continue;
+
+          llvm::Value *lhs = binOp->getOperand(0);
+          llvm::Value *shiftOp = nullptr;
+          llvm::IRBuilder<> builder(binOp);
+          llvm::Value *shiftAmountVal = llvm::ConstantInt::get(
+              binOp->getType(), C->getValue().logBase2());
+
           if (binOp->getOpcode() == llvm::Instruction::Mul) {
-            binOp->dump();
-            llvm::Value *lhs = binOp->getOperand(0);
-            llvm::Value *rhs = binOp->getOperand(1);
-            llvm::IRBuilder<> builder(binOp);
-            llvm::Value *shiftOp = builder.CreateShl(lhs, rhs);
+            shiftOp = builder.CreateShl(lhs, shiftAmountVal, "Shl");
+          } else if (binOp->getOpcode() == llvm::Instruction::UDiv) {
+            shiftOp = builder.CreateLShr(lhs, shiftAmountVal, "Shr");
+          } else if (binOp->getOpcode() == llvm::Instruction::SDiv) {
+            llvm::Type *type = binOp->getType();
+            unsigned width = type->getIntegerBitWidth();
+            if (!C->getValue().isStrictlyPositive())
+              continue;
+            llvm::APInt mask =
+                llvm::APInt::getLowBitsSet(width, C->getValue().logBase2());
+            llvm::Value *zero = llvm::ConstantInt::get(type, 0);
+            llvm::Value *maskOffset = llvm::ConstantInt::get(type, mask);
+            llvm::Value *isNeg = builder.CreateICmpSLT(lhs, zero);
+            llvm::Value *offset = builder.CreateSelect(isNeg, maskOffset, zero);
+            llvm::Value *lhsChanged = builder.CreateAdd(lhs, offset);
+            shiftOp = builder.CreateAShr(lhsChanged, shiftAmountVal, "AShr");
+          }
+          if (shiftOp) {
             binOp->replaceAllUsesWith(shiftOp);
             binOp->eraseFromParent();
+            isChanged = true;
           }
         }
       }
     }
-    return llvm::PreservedAnalyses::all();
+    return isChanged ? llvm::PreservedAnalyses::none()
+                     : llvm::PreservedAnalyses::all();
   }
 
   static bool isRequired() { return true; }
--- llvm/test/compiler-course/papylina_y_fi3/test.ll
+++ llvm/test/compiler-course/papylina_y_fi3/test.ll
@@ -1,5 +1,72 @@
-define dso_local noundef i32 @_Z3mulii(i32 noundef %a, i32 noundef %b){
-entry:
-  %mul = mul nsw i32 %b, %a
+; RUN: opt -load-pass-plugin %llvmshlibdir/papulina_yuliya_fi3_LLVM_IR%pluginext\
+; RUN: -passes=mul-del-optimization -S %s | FileCheck %s
+
+define i32 @_Z4mul1i(i32 noundef %a) {
+; CHECK-LABEL: @_Z4mul1i
+; CHECK-NEXT:  %Shl = shl i32 %a, 2
+; CHECK-NEXT:  ret i32 %Shl
+  %mul = mul nsw i32 %a, 4
   ret i32 %mul
+}
+
+define i32 @_Z4mul3i(i32 noundef %a) {
+; CHECK-LABEL: @_Z4mul3i
+; CHECK-NEXT:  %mul = mul nsw i32 %a, 7
+; CHECK-NEXT:  ret i32 %mul
+  %mul = mul nsw i32 %a, 7
+  ret i32 %mul
+}
+
+define i32 @_Z4mul4i(i32 noundef %a) {
+; CHECK-LABEL: @_Z4mul4i
+; CHECK-NEXT:  %mul = mul nsw i32 %a, -8
+; CHECK-NEXT:  ret i32 %mul
+  %mul = mul nsw i32 %a, -8
+  ret i32 %mul
+}
+
+define i32 @_Z4div1i(i32 noundef %a) {
+; CHECK-LABEL: @_Z4div1i
+; CHECK-NEXT:  %1 = icmp slt i32 %a, 0
+; CHECK-NEXT:  %2 = select i1 %1, i32 7, i32 0
+; CHECK-NEXT:  %3 = add i32 %a, %2
+; CHECK-NEXT:  %AShr = ashr i32 %3, 3
+; CHECK-NEXT:  ret i32 %AShr
+  %div = sdiv i32 %a, 8
+  ret i32 %div
+}
+
+define i32 @_Z4div2i(i32 noundef %a) {
+; CHECK-LABEL: @_Z4div2i
+; CHECK-NEXT:  %div = sdiv i32 %a, -8
+; CHECK-NEXT:  ret i32 %div
+  %div = sdiv i32 %a, -8
+  ret i32 %div
+}
+
+define i32 @_Z4div3i(i32 noundef %a) {
+; CHECK-LABEL: @_Z4div3i
+; CHECK-NEXT:  %1 = icmp slt i32 %a, 0
+; CHECK-NEXT:  %2 = select i1 %1, i32 3, i32 0
+; CHECK-NEXT:  %3 = add i32 %a, %2
+; CHECK-NEXT:  %AShr = ashr i32 %3, 2
+; CHECK-NEXT:  ret i32 %AShr
+  %div = sdiv i32 %a, 4
+  ret i32 %div
+}
+
+define i32 @_Z4div4i(i32 noundef %a) {
+; CHECK-LABEL: @_Z4div4i
+; CHECK-NEXT:  %div = sdiv i32 %a, 3
+; CHECK-NEXT:  ret i32 %div
+  %div = sdiv i32 %a, 3
+  ret i32 %div
+}
+
+define i32 @_Z4div5j(i32 noundef %a) {
+; CHECK-LABEL:  @_Z4div5j
+; CHECK-NEXT: %Shr = lshr i32 %a, 3
+; CHECK-NEXT: ret i32 %Shr
+  %div1 = udiv i32 %a, 8
+  ret i32 %div1
 }
