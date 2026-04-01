44a2720f7deb1a244e12d56255c32506ddefa1b3
yolage-stack <andr24367@mail.ru>
2026-04-01 15:57:43 +0300

    clang fixes

--- llvm/compiler-course/llvm-ir/lobanov_d_lab2/lobanovD5var.cpp
+++ llvm/compiler-course/llvm-ir/lobanov_d_lab2/lobanovD5var.cpp
@@ -12,21 +12,20 @@ struct MulDivToShiftPass : PassInfoMixin<MulDivToShiftPass> {
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
     std::vector<std::pair<Instruction *, BinaryOperator *>> replacements;
 
-
     for (BasicBlock &BB : F) {
       for (Instruction &I : BB) {
 
         auto *BinOp = dyn_cast<BinaryOperator>(&I);
-        if (!BinOp) continue;
+        if (!BinOp)
+          continue;
 
         unsigned Opcode = BinOp->getOpcode();
-        if (Opcode != Instruction::Mul &&
-            Opcode != Instruction::UDiv &&
+        if (Opcode != Instruction::Mul && Opcode != Instruction::UDiv &&
             Opcode != Instruction::SDiv)
           continue;
 
-        ConstantInt *ConstOp = nullptr;  
-        Value *OtherOp = nullptr;         
+        ConstantInt *ConstOp = nullptr;
+        Value *OtherOp = nullptr;
 
         if (Opcode == Instruction::Mul) {
           if ((ConstOp = dyn_cast<ConstantInt>(BinOp->getOperand(0))) &&
@@ -36,9 +35,10 @@ struct MulDivToShiftPass : PassInfoMixin<MulDivToShiftPass> {
                      !isa<ConstantInt>(BinOp->getOperand(0))) {
             OtherOp = BinOp->getOperand(0);
           }
-        } else { 
+        } else {
           ConstOp = dyn_cast<ConstantInt>(BinOp->getOperand(1));
-          if (ConstOp) OtherOp = BinOp->getOperand(0);
+          if (ConstOp)
+            OtherOp = BinOp->getOperand(0);
         }
 
         if (ConstOp && OtherOp) {
@@ -47,19 +47,19 @@ struct MulDivToShiftPass : PassInfoMixin<MulDivToShiftPass> {
             unsigned ShiftAmt = Val.logBase2();
             Type *IntTy = OtherOp->getType();
 
-            Constant *ShiftConst = ConstantInt::get(OtherOp->getType(), ShiftAmt);
+            Constant *ShiftConst =
+                ConstantInt::get(OtherOp->getType(), ShiftAmt);
 
             Instruction::BinaryOps ShiftOp;
             if (Opcode == Instruction::Mul)
-              ShiftOp = Instruction::Shl;  
+              ShiftOp = Instruction::Shl;
             else if (Opcode == Instruction::UDiv)
-              ShiftOp = Instruction::LShr; 
+              ShiftOp = Instruction::LShr;
             else // SDiv
               ShiftOp = Instruction::AShr;
 
-            BinaryOperator *NewInst =
-                BinaryOperator::Create(ShiftOp, OtherOp, ShiftConst,
-                                       BinOp->getName(), &I);
+            BinaryOperator *NewInst = BinaryOperator::Create(
+                ShiftOp, OtherOp, ShiftConst, BinOp->getName(), &I);
             replacements.emplace_back(BinOp, NewInst);
           }
         }
@@ -73,9 +73,8 @@ struct MulDivToShiftPass : PassInfoMixin<MulDivToShiftPass> {
       Old->eraseFromParent();
     }
 
-
     return replacements.empty() ? PreservedAnalyses::all()
-                                 : PreservedAnalyses::none();
+                                : PreservedAnalyses::none();
   }
 
   static bool isRequired() { return true; }
