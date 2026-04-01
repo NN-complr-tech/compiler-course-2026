9f5588f3a7ea3eedd399832277ff2a5ba78e26b0
yolage-stack <andr24367@mail.ru>
2026-04-01 15:23:00 +0300

    working version

---
+++ llvm/compiler-course/llvm-ir/lobanov_d_lab2/lobanovD5var.cpp
@@ -0,0 +1,100 @@
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+
+struct MulDivToShiftPass : PassInfoMixin<MulDivToShiftPass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+    std::vector<std::pair<Instruction *, BinaryOperator *>> replacements;
+
+
+    for (BasicBlock &BB : F) {
+      for (Instruction &I : BB) {
+
+        auto *BinOp = dyn_cast<BinaryOperator>(&I);
+        if (!BinOp) continue;
+
+        unsigned Opcode = BinOp->getOpcode();
+        if (Opcode != Instruction::Mul &&
+            Opcode != Instruction::UDiv &&
+            Opcode != Instruction::SDiv)
+          continue;
+
+        ConstantInt *ConstOp = nullptr;  
+        Value *OtherOp = nullptr;         
+
+        if (Opcode == Instruction::Mul) {
+          if ((ConstOp = dyn_cast<ConstantInt>(BinOp->getOperand(0))) &&
+              !isa<ConstantInt>(BinOp->getOperand(1))) {
+            OtherOp = BinOp->getOperand(1);
+          } else if ((ConstOp = dyn_cast<ConstantInt>(BinOp->getOperand(1))) &&
+                     !isa<ConstantInt>(BinOp->getOperand(0))) {
+            OtherOp = BinOp->getOperand(0);
+          }
+        } else { 
+          ConstOp = dyn_cast<ConstantInt>(BinOp->getOperand(1));
+          if (ConstOp) OtherOp = BinOp->getOperand(0);
+        }
+
+        if (ConstOp && OtherOp) {
+          APInt Val = ConstOp->getValue();
+          if (Val.isPowerOf2() && Val.isNonNegative()) {
+            unsigned ShiftAmt = Val.logBase2();
+            Type *IntTy = OtherOp->getType();
+
+            Constant *ShiftConst = ConstantInt::get(OtherOp->getType(), ShiftAmt);
+
+            Instruction::BinaryOps ShiftOp;
+            if (Opcode == Instruction::Mul)
+              ShiftOp = Instruction::Shl;  
+            else if (Opcode == Instruction::UDiv)
+              ShiftOp = Instruction::LShr; 
+            else // SDiv
+              ShiftOp = Instruction::AShr;
+
+            BinaryOperator *NewInst =
+                BinaryOperator::Create(ShiftOp, OtherOp, ShiftConst,
+                                       BinOp->getName(), &I);
+            replacements.emplace_back(BinOp, NewInst);
+          }
+        }
+      }
+    }
+
+    for (auto &Pair : replacements) {
+      Instruction *Old = Pair.first;
+      BinaryOperator *New = Pair.second;
+      Old->replaceAllUsesWith(New);
+      Old->eraseFromParent();
+    }
+
+
+    return replacements.empty() ? PreservedAnalyses::all()
+                                 : PreservedAnalyses::none();
+  }
+
+  static bool isRequired() { return true; }
+};
+
+} // namespace
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "MulDivToShiftPass", "0.1",
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) -> bool {
+                  if (name == "mul-div-to-shift") {
+                    FPM.addPass(MulDivToShiftPass{});
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
---
+++ llvm/test/compiler-course/lobanov_d_lab2/test_llvm_ir.ll
@@ -0,0 +1,89 @@
+; RUN: opt -load-pass-plugin %llvmshlibdir/lobanov_d_lab2%pluginext -passes=mul-div-to-shift -S %s | FileCheck %s
+
+define i16 @mul_i16_32(i16 %x) {
+; CHECK-LABEL: @mul_i16_32
+; CHECK-NEXT:    [[RES:%.*]] = shl i16 %x, 5
+; CHECK-NEXT:    ret i16 [[RES]]
+  %res = mul i16 %x, 32
+  ret i16 %res
+}
+
+define i64 @udiv_i64_128(i64 %x) {
+; CHECK-LABEL: @udiv_i64_128
+; CHECK-NEXT:    [[RES:%.*]] = lshr i64 %x, 7
+; CHECK-NEXT:    ret i64 [[RES]]
+  %res = udiv i64 %x, 128
+  ret i64 %res
+}
+
+define i32 @mul_by_one(i32 %x) {
+; CHECK-LABEL: @mul_by_one
+; CHECK-NEXT:    [[RES:%.*]] = shl i32 %x, 0
+; CHECK-NEXT:    ret i32 [[RES]]
+  %res = mul i32 %x, 1
+  ret i32 %res
+}
+
+define i32 @udiv_by_one(i32 %x) {
+; CHECK-LABEL: @udiv_by_one
+; CHECK-NEXT:    [[RES:%.*]] = lshr i32 %x, 0
+; CHECK-NEXT:    ret i32 [[RES]]
+  %res = udiv i32 %x, 1
+  ret i32 %res
+}
+
+define i32 @const_left_2(i32 %x) {
+; CHECK-LABEL: @const_left_2
+; CHECK-NEXT:    [[RES:%.*]] = shl i32 %x, 1
+; CHECK-NEXT:    ret i32 [[RES]]
+  %res = mul i32 2, %x
+  ret i32 %res
+}
+
+define i32 @multi_ops(i32 %a, i32 %b, i32 %c) {
+; CHECK-LABEL: @multi_ops
+; CHECK-DAG:     [[A_SHIFT:%.*]] = shl i32 %a, 3
+; CHECK-DAG:     [[B_SHIFT:%.*]] = lshr i32 %b, 4
+; CHECK-DAG:     [[C_SHIFT:%.*]] = ashr i32 %c, 2
+; CHECK-NEXT:    [[SUM1:%.*]] = add i32 [[A_SHIFT]], [[B_SHIFT]]
+; CHECK-NEXT:    [[SUM2:%.*]] = add i32 [[SUM1]], [[C_SHIFT]]
+; CHECK-NEXT:    ret i32 [[SUM2]]
+  %mul_a = mul i32 %a, 8
+  %udiv_b = udiv i32 %b, 16
+  %sdiv_c = sdiv i32 %c, 4
+  %sum = add i32 %mul_a, %udiv_b
+  %total = add i32 %sum, %sdiv_c
+  ret i32 %total
+}
+
+define i32 @mul_negative_power(i32 %x) {
+; CHECK-LABEL: @mul_negative_power
+; CHECK-NEXT:    [[RES:%.*]] = mul i32 %x, -16
+; CHECK-NEXT:    ret i32 [[RES]]
+  %res = mul i32 %x, -16
+  ret i32 %res
+}
+
+define i32 @mul_not_pow2_6(i32 %x) {
+; CHECK-LABEL: @mul_not_pow2_6
+; CHECK-NEXT:    [[RES:%.*]] = mul i32 %x, 6
+; CHECK-NEXT:    ret i32 [[RES]]
+  %res = mul i32 %x, 6
+  ret i32 %res
+}
+
+define i32 @mixed_replace(i32 %a, i32 %b) {
+; CHECK-LABEL: @mixed_replace
+; CHECK:         [[MUL1:%.*]] = shl i32 %a, 1
+; CHECK-NEXT:    [[MUL2:%.*]] = mul i32 %b, 7
+; CHECK-NEXT:    [[DIV:%.*]] = ashr i32 %b, 3
+; CHECK-NEXT:    [[ADD1:%.*]] = add i32 [[MUL1]], [[MUL2]]
+; CHECK-NEXT:    [[RES:%.*]] = add i32 [[ADD1]], [[DIV]]
+; CHECK-NEXT:    ret i32 [[RES]]
+  %mul1 = mul i32 %a, 2
+  %mul2 = mul i32 %b, 7
+  %div = sdiv i32 %b, 8
+  %add = add i32 %mul1, %mul2
+  %result = add i32 %add, %div
+  ret i32 %result
+}
