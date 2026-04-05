23fd51b8f6972d53ffe8964cc7770a8c35226bb3
smallAbyss <eal1105@yandex.ru>
2026-04-05 22:59:01 +0300

    [DEV] implement fmuladd_intrinsic pass

--- llvm/compiler-course/llvm-ir/luzan_e_fmuladd/fmulladd.cpp
+++ llvm/compiler-course/llvm-ir/luzan_e_fmuladd/fmulladd.cpp
@@ -3,13 +3,36 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/Intrinsics.h"
 
 namespace {
 struct LE_FmullAddDecPass : llvm::PassInfoMixin<LE_FmullAddDecPass> {
   llvm::PreservedAnalyses run(llvm::Function &func,
                               llvm::FunctionAnalysisManager &) {
-    llvm::outs() << func.getName() << '\n';
-    return llvm::PreservedAnalyses::all();
+    bool changed = false;
+    for (llvm::BasicBlock &basic_block : func) {
+      for (llvm::Instruction &instr : llvm::make_early_inc_range(basic_block)) {
+        if(llvm::IntrinsicInst* intrinsic = llvm::dyn_cast<llvm::IntrinsicInst>(&instr)) {
+          if (intrinsic->getIntrinsicID() == llvm::Intrinsic::fmuladd) {
+            llvm::Value* a = intrinsic->getArgOperand(0); 
+            llvm::Value* b = intrinsic->getArgOperand(1);
+            llvm::Value* c = intrinsic->getArgOperand(2);
+
+            llvm::IRBuilder<> builder(intrinsic);
+            llvm::Value* mul_res = builder.CreateFMul(a, b, "fmul"); 
+            llvm::Value* add_res = builder.CreateFAdd(mul_res, c, "fadd");
+
+            intrinsic->replaceAllUsesWith(add_res);
+            intrinsic->eraseFromParent();
+            changed = true;
+          }
+        }
+      }
+    }
+
+    return changed ? llvm::PreservedAnalyses::none() : llvm::PreservedAnalyses::all();
   }
 
   static bool isRequired() { return true; }
