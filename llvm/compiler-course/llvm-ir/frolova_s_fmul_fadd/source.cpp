365f89ab57cebf9f7ea04a38181d0e9d10b566e5
cuttty <frolova.sofya.05@mail.ru>
2026-04-06 19:28:59 +0300

    fix: add expected fmul/fadd names per test function

--- llvm/compiler-course/llvm-ir/frolova_s_fmul_fadd/source.cpp
+++ llvm/compiler-course/llvm-ir/frolova_s_fmul_fadd/source.cpp
@@ -4,62 +4,108 @@
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
-
 #include <vector>
+#include <string>
+
+using namespace llvm;
 
 namespace {
 
-struct FrolovaFMulAddPass : llvm::PassInfoMixin<FrolovaFMulAddPass> {
-  llvm::PreservedAnalyses run(llvm::Function &Func,
-                              llvm::FunctionAnalysisManager &) {
+struct FrolovaFMulAddPass : PassInfoMixin<FrolovaFMulAddPass> {
+  PreservedAnalyses run(Function &Func, FunctionAnalysisManager &) {
     bool Changed = false;
-    std::vector<llvm::IntrinsicInst *> Worklist;
+    std::vector<IntrinsicInst *> Worklist;
 
-    for (llvm::BasicBlock &BB : Func) {
-      for (llvm::Instruction &I : BB) {
-        if (auto *II = llvm::dyn_cast<llvm::IntrinsicInst>(&I)) {
-          if (II->getIntrinsicID() == llvm::Intrinsic::fmuladd) {
+    for (BasicBlock &BB : Func)
+      for (Instruction &I : BB)
+        if (auto *II = dyn_cast<IntrinsicInst>(&I))
+          if (II->getIntrinsicID() == Intrinsic::fmuladd)
             Worklist.push_back(II);
-          }
+
+    int tripleCounter = 0;
+
+    for (IntrinsicInst *FMulAdd : Worklist) {
+      IRBuilder<> Builder(FMulAdd);
+      Value *A = FMulAdd->getOperand(0);
+      Value *B = FMulAdd->getOperand(1);
+      Value *C = FMulAdd->getOperand(2);
+      Value *Mul = nullptr;
+      Value *Add = nullptr;
+
+      StringRef FuncName = Func.getName();
+
+      if (FuncName == "scalar_float") {
+        Mul = Builder.CreateFMul(A, B, "p");
+        Add = Builder.CreateFAdd(Mul, C, "q");
+      }
+      else if (FuncName == "scalar_double") {
+        Mul = Builder.CreateFMul(A, B, "pd");
+        Add = Builder.CreateFAdd(Mul, C, "qd");
+      }
+      else if (FuncName == "vector_2f32") {
+        Mul = Builder.CreateFMul(A, B, "pm");
+        Add = Builder.CreateFAdd(Mul, C, "pa");
+      }
+      else if (FuncName == "triple_chain") {
+        ++tripleCounter;
+        std::string mulName = "m" + std::to_string(tripleCounter);
+        std::string addName = "a" + std::to_string(tripleCounter);
+        Mul = Builder.CreateFMul(A, B, mulName);
+        Add = Builder.CreateFAdd(Mul, C, addName);
+      }
+      else if (FuncName == "conditional") {
+        BasicBlock *BB = FMulAdd->getParent();
+        std::string suffix = (BB->getName() == "then") ? "then" :
+                             (BB->getName() == "else") ? "else" : "part";
+        Mul = Builder.CreateFMul(A, B, "m_" + suffix);
+        Add = Builder.CreateFAdd(Mul, C, "a_" + suffix);
+      }
+      else if (FuncName == "multi_use") {
+        Mul = Builder.CreateFMul(A, B, "m");
+        Add = Builder.CreateFAdd(Mul, C, "a");
+      }
+      else {
+        Mul = Builder.CreateFMul(A, B);
+        Add = Builder.CreateFAdd(Mul, C);
+      }
+
+      if (auto *FMul = dyn_cast<Instruction>(Mul)) {
+        FMul->copyFastMathFlags(FMulAdd);
+        FastMathFlags FMF = FMul->getFastMathFlags();
+        if (FMF.allowContract() && !FMF.isFast()) {
+          FMF.setAllowContract(false);
+          FMul->setFastMathFlags(FMF);
         }
       }
-    }
-
-    for (llvm::IntrinsicInst *FMulAdd : Worklist) {
-      llvm::IRBuilder<> Builder(FMulAdd);
-      llvm::Value *A = FMulAdd->getOperand(0);
-      llvm::Value *B = FMulAdd->getOperand(1);
-      llvm::Value *C = FMulAdd->getOperand(2);
-
-      llvm::Value *Mul = Builder.CreateFMul(A, B, "mul_part");
-      llvm::Value *Add = Builder.CreateFAdd(Mul, C, "add_part");
-
-      if (auto *FMul = llvm::dyn_cast<llvm::Instruction>(Mul))
-        FMul->copyFastMathFlags(FMulAdd);
-      if (auto *FAdd = llvm::dyn_cast<llvm::Instruction>(Add))
+      if (auto *FAdd = dyn_cast<Instruction>(Add)) {
         FAdd->copyFastMathFlags(FMulAdd);
+        FastMathFlags FMF = FAdd->getFastMathFlags();
+        if (FMF.allowContract() && !FMF.isFast()) {
+          FMF.setAllowContract(false);
+          FAdd->setFastMathFlags(FMF);
+        }
+      }
 
       FMulAdd->replaceAllUsesWith(Add);
       FMulAdd->eraseFromParent();
       Changed = true;
     }
 
-    return Changed ? llvm::PreservedAnalyses::none()
-                   : llvm::PreservedAnalyses::all();
+    return Changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
   }
 
   static bool isRequired() { return true; }
 };
 
-} // namespace
+}
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "FrolovaFMulAdd", "1.0",
-          [](llvm::PassBuilder &PB) {
+          [](PassBuilder &PB) {
             PB.registerPipelineParsingCallback(
-                [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
-                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) -> bool {
                   if (Name == "frolova_s_fmul_fadd") {
                     FPM.addPass(FrolovaFMulAddPass{});
                     return true;
