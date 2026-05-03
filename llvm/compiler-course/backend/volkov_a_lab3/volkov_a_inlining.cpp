243e292e78266cf1a62f64a2acee6ac6ad6808e3
fleezeex <leshavolkovv@gmail.com>
2026-05-03 20:14:15 +0300

    format fix

--- llvm/compiler-course/backend/volkov_a_lab3/volkov_a_inlining.cpp
+++ llvm/compiler-course/backend/volkov_a_lab3/volkov_a_inlining.cpp
@@ -9,86 +9,87 @@
 using namespace llvm;
 
 namespace {
-    constexpr unsigned MaxInsts = 15;
-    [[maybe_unused]] constexpr unsigned MaxDepth = 3;
+constexpr unsigned MaxInsts = 15;
+[[maybe_unused]] constexpr unsigned MaxDepth = 3;
 
-    // Подсчет инструкций
-    unsigned getIRSize(const Function *F) {
-        unsigned Cnt = 0;
-        for (const auto &BB : *F) {
-            Cnt += BB.size();
-        }
-        return Cnt;
-    }
-
-    bool checkRecursion(const Function *F) {
-        for (const auto &BB : *F) {
-            for (const auto &I : BB) {
-                if (const auto *Call = dyn_cast<CallBase>(&I)) {
-                    if (Call->getCalledFunction() == F) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
+// Подсчет инструкций
+unsigned getIRSize(const Function *F) {
+  unsigned Cnt = 0;
+  for (const auto &BB : *F) {
+    Cnt += BB.size();
+  }
+  return Cnt;
 }
+
+bool checkRecursion(const Function *F) {
+  for (const auto &BB : *F) {
+    for (const auto &I : BB) {
+      if (const auto *Call = dyn_cast<CallBase>(&I)) {
+        if (Call->getCalledFunction() == F) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+} // namespace
 
 namespace {
 
 class VolkovInliner : public MachineFunctionPass {
 public:
-    static char ID;
-    VolkovInliner() : MachineFunctionPass(ID) {}
+  static char ID;
+  VolkovInliner() : MachineFunctionPass(ID) {}
 
-    bool runOnMachineFunction(MachineFunction &MF) override {
-        SmallVector<MachineInstr*, 8> ToDelete;
+  bool runOnMachineFunction(MachineFunction &MF) override {
+    SmallVector<MachineInstr *, 8> ToDelete;
 
-        for (auto &MBB : MF) {
-            for (auto &MI : MBB) {
-                if (MI.isCall() && canBeInlined(MI)) {
-                    ToDelete.push_back(&MI);
-                }
-            }
+    for (auto &MBB : MF) {
+      for (auto &MI : MBB) {
+        if (MI.isCall() && canBeInlined(MI)) {
+          ToDelete.push_back(&MI);
         }
-
-        for (auto *CallInstr : ToDelete) {
-            CallInstr->eraseFromParent();
-        }
-
-        return !ToDelete.empty();
+      }
     }
+
+    for (auto *CallInstr : ToDelete) {
+      CallInstr->eraseFromParent();
+    }
+
+    return !ToDelete.empty();
+  }
 
 private:
-    bool canBeInlined(MachineInstr &CallInst) {
-        const Function *FuncToCall = nullptr;
+  bool canBeInlined(MachineInstr &CallInst) {
+    const Function *FuncToCall = nullptr;
 
-        for (const auto &Op : CallInst.operands()) {
-            if (Op.isGlobal()) {
-                FuncToCall = dyn_cast<Function>(Op.getGlobal());
-                if (FuncToCall) break;
-            }
-        }
-
-        if (!FuncToCall || FuncToCall->isDeclaration()) {
-            return false;
-        }
-
-        if (getIRSize(FuncToCall) > MaxInsts) {
-            return false;
-        }
-
-        if (checkRecursion(FuncToCall)) {
-            return false; 
-        }
-
-        return true;
+    for (const auto &Op : CallInst.operands()) {
+      if (Op.isGlobal()) {
+        FuncToCall = dyn_cast<Function>(Op.getGlobal());
+        if (FuncToCall)
+          break;
+      }
     }
+
+    if (!FuncToCall || FuncToCall->isDeclaration()) {
+      return false;
+    }
+
+    if (getIRSize(FuncToCall) > MaxInsts) {
+      return false;
+    }
+
+    if (checkRecursion(FuncToCall)) {
+      return false;
+    }
+
+    return true;
+  }
 };
 
 char VolkovInliner::ID = 0;
 
-}
+} // namespace
 
 RegisterPass<VolkovInliner> X("volkov-a-inline", "inlining_pass_volkov_a");
