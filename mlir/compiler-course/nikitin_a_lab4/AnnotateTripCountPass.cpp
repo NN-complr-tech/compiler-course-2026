8727a825451e8ab374bf2952d9ad1d122e6f0304
Anton Nikitin <anikitin52@yandex.ru>
2026-05-09 12:04:48 +0300

    clang-format fix

--- mlir/compiler-course/nikitin_a_lab4/AnnotateTripCountPass.cpp
+++ mlir/compiler-course/nikitin_a_lab4/AnnotateTripCountPass.cpp
@@ -9,40 +9,42 @@
 using namespace mlir;
 
 namespace {
-class AnnotateTripCountPass : public PassWrapper<AnnotateTripCountPass, OperationPass<ModuleOp>> {
+class AnnotateTripCountPass
+    : public PassWrapper<AnnotateTripCountPass, OperationPass<ModuleOp>> {
 public:
   StringRef getArgument() const final { return "nikitin_a_lab4_MLIR"; }
-  StringRef getDescription() const final { 
-    return "Annotate affine.for loops with trip_count attribute representing the number of iterations"; 
+  StringRef getDescription() const final {
+    return "Annotate affine.for loops with trip_count attribute representing "
+           "the number of iterations";
   }
 
   void runOnOperation() override {
     ModuleOp moduleOp = getOperation();
-    
+
     moduleOp.walk([&](affine::AffineForOp forOp) {
       // Check if bounds are constant
       if (!forOp.hasConstantLowerBound() || !forOp.hasConstantUpperBound()) {
         return;
       }
-      
+
       int64_t lowerBound = forOp.getConstantLowerBound();
       int64_t upperBound = forOp.getConstantUpperBound();
       int64_t step = forOp.getStepAsInt();
-      
+
       // Calculate trip count
       int64_t range = upperBound - lowerBound;
       int64_t tripCount;
-      
+
       if (range <= 0) {
         tripCount = 0;
       } else {
         tripCount = (range + step - 1) / step;
       }
-      
+
       // Add attribute to the operation
-      forOp->setAttr("trip_count", 
-                     IntegerAttr::get(IntegerType::get(forOp.getContext(), 64), 
-                                     tripCount));
+      forOp->setAttr("trip_count",
+                     IntegerAttr::get(IntegerType::get(forOp.getContext(), 64),
+                                      tripCount));
     });
   }
 };
