eff88d28d018df9f968d5faf19e8db42c364ac08
fleezeex <leshavolkovv@gmail.com>
2026-05-10 19:47:20 +0300

    one more fix....

--- llvm/compiler-course/backend/volkov_a_lab3/volkov_a_inlining.cpp
+++ llvm/compiler-course/backend/volkov_a_lab3/volkov_a_inlining.cpp
@@ -90,4 +90,4 @@ char VolkovInliner::ID = 0;
 
 } // namespace
 
-RegisterPass<VolkovInliner> X("volkov-inline", "Volkov Inliner Pass");
+RegisterPass<VolkovInliner> X("volkov-a-inline", "Volkov Inliner Pass");
