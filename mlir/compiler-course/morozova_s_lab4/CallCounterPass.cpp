73b1eaf56569d9965db8a69a45e68572bb6731bf
SonyaMorozova <sonyasmorozova@gmail.com>
2026-05-13 19:17:01 +0300

    fix gcc-build

--- mlir/compiler-course/morozova_s_lab4/CallCounterPass.cpp
+++ mlir/compiler-course/morozova_s_lab4/CallCounterPass.cpp
@@ -2,7 +2,6 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Interfaces/CallInterfaces.h"
 #include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassManager.h"
 #include "llvm/ADT/StringMap.h"
 #include "llvm/Support/Compiler.h"
 
@@ -46,24 +45,29 @@ public:
 
 namespace mlir {
 namespace compiler_course {
-
 std::unique_ptr<Pass> createCallCounterPass() {
   return std::make_unique<CallCounterPass>();
 }
-
 } // namespace compiler_course
 } // namespace mlir
 
+#ifndef MLIR_PLUGIN_API_VERSION
 #define MLIR_PLUGIN_API_VERSION 1
+#endif
+
+namespace mlir {
+struct PassPluginLibraryInfo {
+  int apiVersion;
+  const char *pluginName;
+  const char *pluginVersion;
+  void (*registerPassRegistryCallbacks)(PassRegistry &);
+};
+} // namespace mlir
 
 extern "C" LLVM_ATTRIBUTE_WEAK mlir::PassPluginLibraryInfo
 mlirGetPassPluginInfo() {
-  struct PassPluginLibraryInfo info;
-  info.apiVersion = MLIR_PLUGIN_API_VERSION;
-  info.pluginName = "call_counter";
-  info.pluginVersion = "0.1";
-  info.registerPassRegistryCallback = [](mlir::PassRegistry &registry) {
-    registry.addPass(mlir::compiler_course::createCallCounterPass());
-  };
-  return info;
+  return {MLIR_PLUGIN_API_VERSION, "call_counter", "0.1",
+          [](mlir::PassRegistry &registry) {
+            registry.addPass(mlir::compiler_course::createCallCounterPass());
+          }};
 }
